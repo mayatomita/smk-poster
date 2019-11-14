fc01402ba80b910ea84408a5fa260574494b93fa
viko <viko@itu.dk>
2019-11-14 13:00:17 +0100

    colorMap
    :non-potable_water:

--- script.js
+++ script.js
@@ -3,13 +3,32 @@
 getImagebyID()
 searchForArtist()
 
+var colorMap = new Map()
+colorMap.set("LILLA", ["863B7A","670067","400572", "8B19E6", "CEA3F5", "9796FF"])
+colorMap.set("LYSERØD", ["FF75E9", "FF00FF","FFB6F3","FF96FF","FFB2BE"])
+colorMap.set("RØD", ["FF9897","820010","670000","FF1139","FF0000"])
+colorMap.set("ORANGE", ["713806","674200","E26F03","FFA600","F4C6A3"])
+colorMap.set("GUL", ["FFFF00","FFDC95","FFFF93"])
+colorMap.set("GRØN", ["666700","006700","00FF00","95FF95"])
+colorMap.set("BLÅ", ["037676","00FEFF","88FFFF","004F87","0097FF","7BC9FF","000068", "0000FF"])
+colorMap.set("SORT", ["000000"])
+colorMap.set("GRÅ", ["555555","5A5A5A","9B9B9B","2D2D2D"])
+colorMap.set("HVID", ["FFFFFF"])
+
+
+
 function searchForImage (color) {
 
   //Make a variable for the request to the API of the type XMLHttpRequest
   var request = new XMLHttpRequest()
 
+  //Choose random color within the selected "Main" colors.
+  var lsColor =  colorMap.get(color)
+  var randomIndex = Math.floor(Math.random() * lsColor.length)
+  var colorToUse = lsColor[randomIndex]
+
   //Prepare the request
-  request.open('GET', 'https://api.smk.dk/api/v1/art/search/?keys=*&filters=%5Bcolors%3A%23' + color + '%5D' , true)
+  request.open('GET', 'https://api.smk.dk/api/v1/art/search/?keys=*&filters=%5Bcolors%3A%23' + colorToUse + '%5D' , true)
 
   //This code will run when we get an answer to the request
   request.onload = function() {
@@ -38,7 +57,6 @@ function searchForImage (color) {
         img.src = thumbnailUrl
         //Put the new <img> element into the element with the ID "search images"
         document.getElementById("search-images").appendChild(img)
-      }
 
     //If the request was not successful write error in the console
     } else {
